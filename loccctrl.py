857c6636f232bbafb872c8c1599a669ec6977d87
jaseg <code@jaseg.net>
2014-07-10 15:29:12 +0200

    Fixed logging

--- loccctrl.py
+++ loccctrl.py
@@ -6,9 +6,15 @@ import serial
 import time
 import threading
 import functools
+import os
+import sys
 from lmap import *
 
 import config
+
+def log(*args):
+	print(*args)
+	sys.stdout.flush()
 
 PIN_CACHE = {}
 
@@ -31,6 +37,14 @@ class HardwareInterface:
 		with self.lock:
 			self.ser.write(b'\no\n')
 			self.ser.read(4)
+
+	def try_open(self):
+		if not self.lock.acquire(False):
+			return False
+		self.ser.write(b'\no\n')
+		self.ser.read(4)
+		self.lock.release()
+		return True
 	
 	def readcmd(self):
 		with self.lock:
@@ -51,51 +65,51 @@ def pwcheck(record, pw):
 	return hashv == newhashv
 
 def test_access(uid, pin):
-	print('Looking up user', uid)
+	log('Looking up user', uid)
 	try:
 		lm = ldap_connect()
 		user = lm(config.LDAP.USERBASE).search(config.LDAP.ACCESS_FILTER.format(uid))[0]
 		PIN_CACHE[uid] = user[config.LDAP.PINFIELD]
 	except Exception as e:
-		print('Invalid user/pin:', uid, '('+str(e)+')')
+		log('Invalid user/pin:', uid, '('+str(e)+')')
 	if pwcheck(PIN_CACHE.get(uid, config.BACKUP_PIN), pin):
 		return True
 	return False
 
-print('Starting up...')
+log('Starting up...')
 hw = HardwareInterface()
 hw.set_led(0, False) # green
 hw.set_led(1, False) # red
 hw.set_led(2, True) # yellow
-print('Hardware interface initialized.')
+log('Hardware interface initialized.')
 
-nums = list(map(str, range(10)))
-
-numbuf = ''
-while True:
-	cmd = str(hw.readcmd(), 'ASCII')
-	#print('CMD:', cmd, 'numbuf:', numbuf)
-	hw.blink_led(2, 0.1, False) # yellow
-	if cmd in nums:
-		numbuf += cmd
-	if cmd in ['a', 'H'] and len(numbuf) > 4:
-		print('Checking access...')
-		uid = numbuf[:4]
-		pin = numbuf[4:]
-		if(test_access(uid, pin)):
-			print('Access granted')
-			hw.set_led(0, True) # green
-			hw.open()
-			hw.set_led(0, False) # green
+if __name__ == '__main__':
+	nums = list(map(str, range(10)))
+	numbuf = ''
+	while True:
+		cmd = str(hw.readcmd(), 'ASCII')
+		#log('CMD:', cmd, 'numbuf:', numbuf)
+		hw.blink_led(2, 0.1, False) # yellow
+		if cmd in nums:
+			numbuf += cmd
+		if cmd in ['a', 'H'] and len(numbuf) > 4:
+			log('Checking access...')
+			uid = numbuf[:4]
+			pin = numbuf[4:]
+			if(test_access(uid, pin)):
+				log('Access granted')
+				hw.set_led(0, True) # green
+				hw.open()
+				hw.set_led(0, False) # green
+				numbuf = ''
+			else:
+				for i in range(10):
+					hw.blink_led(1, 0.1) # red
+					time.sleep(0.1)
+				log('Access denied')
+				numbuf = ''
+		if cmd in ['c', 'h']:
+			log('Aborted.')
+			hw.blink_led(1, 1.0) # red
 			numbuf = ''
-		else:
-			for i in range(10):
-				hw.blink_led(1, 0.1) # red
-				time.sleep(0.1)
-			print('Access denied')
-			numbuf = ''
-	if cmd in ['c', 'h']:
-		print('Aborted.')
-		hw.blink_led(1, 1.0) # red
-		numbuf = ''
 
